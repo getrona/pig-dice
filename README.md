d75940d0997e93f5b37f6680d0a1749cab7d9519
Getro Naissance and James Lee <getro@>
2016-11-02 16:45:47 -0700

    To be continued for Pig Dice.

---
+++ README.md
@@ -0,0 +1,38 @@
+# _Pig Dice_
+
+#### _{Brief description of application}, November 2nd, 2016_
+
+#### By _**Getro Naissance and James Lee**_
+
+## Description
+
+* If the player rolls a 1, they score nothing and it becomes the next player's turn.
+* If the player rolls any other number, it is added to their turn total and the player's turn continues.
+* If a player chooses to "hold", their turn total is added to their score, and it becomes the next player's turn.
+* The first player to score 100 or more points wins.
+
+## Setup/Installation Requirements
+
+* __
+* __
+* __
+*
+*
+
+## Known Bugs
+
+_{Are there issues that have not yet been resolved that you want to let users know you know?  Outline any issues that would impact use of your application.  Share any workarounds that are in place. }_
+
+## Support and contact details
+
+_{Let people know what to do if they run into any issues or have questions, ideas or concerns.  Encourage them to contact you or make a contribution to the code.}_
+
+## Technologies Used
+
+_{Tell me about the languages and tools you used to create this app. Assume that I know you probably used HTML and CSS. If you did something really cool using only HTML, point that out.}_
+
+### License
+
+*{Determine the license under which this application can be used.  See below for more details on licensing.}*
+
+Copyright (c) 2016 **_{List of contributors or company name}_**
--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,48 @@
 // business logic
-var winningScore = 100;
-var player1Score = 0;
-var player2Score = 0;
-var dice = function() {
-	return Math.floor(Math.random() * 6 + (1));
+function Player(score, winningScore) {
+  this.score = 0;
+  this.winningScore = 100;
+}
+
+var player1 = new Player ();
+var player2 = new Player ();
+
+Player.prototype.singleRoll = function() {
+  return Math.floor(Math.random() * 6 + (1));
 };
+Player.prototype.addScore=function(score){
+  return this.score=+score;
+}
+
+for()
+function() {
+  var roll = dice();
+    if(roll > 1) {
+      player1.addScore(roll);
+    }
+    return player1Score;
+  };
+
+  var player2Game = function() {
+      var roll = dice();
+      if(roll > 1) {
+        player2Score += roll;
+      }
+    return player2Score;
+  };
 
 // user interface logic
+
 $(document).ready(function() {
   $(".btn-success").click(function(event) {
     event.preventDefault();
+      while (player1.score <= 100 && player2.score <= 100) {
 
-    alert(dice());
+      }
   });
 });
+
+
 
 // do {
 //   var player1Game = function() {
@@ -45,3 +74,23 @@ $(document).ready(function() {
 //   };
 // }
 // while (player1Score !== 100 || player2Score !== 100);
+
+
+
+
+    //
+    // while (player1Score <= 100 || player2Score <= 100) {
+    //   player1Game();
+    //   var decision = confirm("Player 1, Do you want to continue or hold?");
+    //   if (decision === true) {
+    //     player1Game();
+    //   } else {
+    //       player2Game();
+    //       var decision = confirm("Player 2, Do you want to continue or hold?");
+    //       if (decision === true) {
+    //         player2Game();
+    //       } else {
+    //         player1Game();
+    //       }
+    //     }
+    // }
